87599e5eebdae910db6355fc26f728f6001f1b2d
RuslanLaptev98 <ruslanlaptev98@gmail.com>
2021-09-14 21:16:12 +0300

    added full article, removed api for production

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -2,14 +2,17 @@ import React from 'react'
 import './Article.css'
 import { AiOutlineArrowRight } from 'react-icons/ai'
 
-export default function Article({ news }) {
+export default function Article({ news, setSelectedArticle }) {
     return (
         <div className="Article">
             <div className="Article__card">
                 <div className="Article__title">{news.title}</div>
                 <div className="Article__author">{news.author}</div>
                 <div className="Article__content">{news.content}</div>
-                <div className="Article__readFull">
+                <div
+                    className="Article__readFull"
+                    onClick={() => setSelectedArticle(news)}
+                >
                     <p className="Article__readFullText">read full article</p>
                     <AiOutlineArrowRight fill="#00a8cc" size="1.4rem" />
                 </div>
--- src/pages/CryptocurrencyNews/CryptocurrencyNews.js
+++ src/pages/CryptocurrencyNews/CryptocurrencyNews.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import './CryptocurrencyNews.css'
 import Article from '../../components/Article/Article'
+import ArticleFull from '../../components/ArticleFull/ArticleFull'
 
 export default function CryptocurrencyNews() {
     const sampleData = [
-        /*
         {
             title: 'Dogecoin: pure genius marketing?',
             author: 'Dave Smith',
@@ -52,40 +52,60 @@ export default function CryptocurrencyNews() {
             author: 'John Doe',
             content:
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        },*/
+        },
     ]
     const [newsData, setNewsData] = useState(sampleData)
+    /*
     fetch(
-        'https://newsapi.org/v2/everything?q=cryptocurrency&from=2021-08-03&sortBy=popularity&apiKey=eac972808180447da763b302e9a2a9a8'
+        'https://newsapi.org/v2/everything?q=cryptocurrency&sortBy=popularity&apiKey=eac972808180447da763b302e9a2a9a8'
     )
         .then((res) => res.json())
         .then((data) => {
             setNewsData(data.articles)
             console.log(data)
         })
-
+        */
     /* Pagination */
     const [visible, setVisible] = useState(4)
     const loadMoreArticles = () => {
         setVisible((prevValue) => prevValue + 4)
     }
+
+    /* State for article */
+    const [selectedArticle, setSelectedArticle] = useState(null)
     return (
         <div className="CryptocurrencyNews">
-            <p className="CryptocurrencyNews__title">
-                Latest opinions of experts and journalists on cryptocurrency.
-            </p>
-            <div className="CryptocurrencyNews__articles">
-                {newsData &&
-                    newsData
-                        .slice(0, visible)
-                        .map((news, i) => <Article key={i} news={news} />)}
-            </div>
-            <button
-                className="CryptocurrencyNews__paginationButton"
-                onClick={() => loadMoreArticles()}
-            >
-                Load more
-            </button>
+            {!selectedArticle ? (
+                <>
+                    <p className="CryptocurrencyNews__title">
+                        Latest opinions of experts and journalists on
+                        cryptocurrency.
+                    </p>
+                    <div className="CryptocurrencyNews__articles">
+                        {newsData &&
+                            newsData
+                                .slice(0, visible)
+                                .map((news, i) => (
+                                    <Article
+                                        key={i}
+                                        news={news}
+                                        setSelectedArticle={setSelectedArticle}
+                                    />
+                                ))}
+                    </div>
+                    <button
+                        className="CryptocurrencyNews__paginationButton"
+                        onClick={() => loadMoreArticles()}
+                    >
+                        Load more
+                    </button>
+                </>
+            ) : (
+                <ArticleFull
+                    selectedArticle={selectedArticle}
+                    setSelectedArticle={setSelectedArticle}
+                />
+            )}
         </div>
     )
 }
